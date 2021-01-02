48120e922778fed867d5d368c707f4c0924c4512
Hisham Ouberkni <37296813+HishamYUM@users.noreply.github.com>
2021-01-02 02:15:17 +0100

    Update functions.h
    add functions prototypes

--- functions.h
+++ functions.h
@@ -1,10 +1,10 @@
 #ifndef FUNCTIONS_H_INCLUDED
-#define FUNCTIONS_H_INCLUDED
+#define FUNCTIONS_H_INCLUDED// include guard pour eviter les problemes des include
 #include <stdio.h>
 #include <stdio.h>
 
 
-
+void yourChoices(char **choices);// afficher les elements choisi
 void showCategories(char **categ);
 void showSubCategories(char **sub_categ);
 void showDesserts(char **d);
@@ -14,5 +14,9 @@ void showTeas(char **t);
 void showTraditionalDishes(char **td);
 void showBurgers(char **b);
 void showOthers(char **o);
-
+void addElements(char **choices, char **categ, int nb);/*ajouter chaque element choisi par l'utilisateur
+                                                        dans l'array choices afin de le conserver pour les utiliser
+                                                        plus tard*/
+void addPrices(double prices[], double categP[], int nb);
+void facture(char **choices, double prices[]);
 #endif // FUNCTIONS_H_INCLUDED
